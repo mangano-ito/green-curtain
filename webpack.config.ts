3f491b7d4e0730f7b81dcaa6494c49ccc99484a3
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2020-04-19 01:09:40 +0900

    add webpack.config

---
+++ webpack.config.ts
@@ -0,0 +1,51 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: {
+        '__wds': 'webpack-dev-server/client?http://localhost:8080',
+        'main': path.join(__dirname, '/src/main.ts'),
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        publicPath: path.join(__dirname, '/dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(glsl)$/,
+                loader: 'raw-loader',
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            '#': path.join(__dirname, '/src'),
+        }
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'assets/index.html',
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        hot: true,
+        port: 8080,
+        open: true,
+    }
+};
+
+export default config;
